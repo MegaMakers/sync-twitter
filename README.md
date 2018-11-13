6c8d97553025dfe69afb00afe8306b617e36d352
Henning Leutz <leutz@pcsg.de>
2018-11-13 20:30:27 +0100

    refactor: start project

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# sync-twitter
+MegaMaker Twitter List Sync
+======
+
+This little app fetches all twitter accounts from the MegaMaker club into a Twitter List
+
+
+Installation
+------
+
+1. Install all requirements with composers
+
+
+```
+composer install
+```
+
+
+2. Setup the Twitter stuff
+
+create a etc/settings.php file
+
+```php
+<?php
+
+return [
+    'oauth_access_token' => "YOUR_OAUTH_ACCESS_TOKEN",
+    'oauth_access_token_secret' => "YOUR_OAUTH_ACCESS_TOKEN_SECRET",
+    'consumer_key' => "YOUR_CONSUMER_KEY",
+    'consumer_secret' => "YOUR_CONSUMER_SECRET"
+];
+
+```
---
+++ getTwitterNames.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * This file fetches all twitter accounts from the MegaMaker club
+ * and outputs these data as json
+ *
+ * @author Henning Leutz (https://github.com/dehenne)
+ */
+
+// helper functions
+
+/**
+ * @param string $url
+ * @return string
+ */
+function fetch($url)
+{
+    $ch = curl_init();
+    $timeout = 5;
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+/**
+ * @param $username
+ * @return string
+ */
+function fetchUser($username)
+{
+    return fetch('https://club.megamaker.co/admin/users/' . $username . '.json)');
+}
+
+/**
+ * STUFF
+ */
+
+$twitterUser = [];
+
+
+// get all users
+
+$call = 'https://club.megamaker.co/admin/users/list/active.json';
+$result = fetch($call);
+$users = json_decode($result, true);
+
+
+// get the profiles of each user
+
+foreach ($users as $username) {
+    $userData = fetchUser($username);
+    $userData = json_decode($userData, true);
+
+    if (!$userData) {
+        continue;
+    }
+
+    if (!isset($userData['user_fields']) || !$userData['user_fields'][1]) {
+        continue;
+    }
+
+    $twitterUser[] = $userData['user_fields'][1];
+}
+
+echo json_encode($twitterUser);
